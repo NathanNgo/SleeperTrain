a7af16c4dad38250d96b6d567d66908ba2d77581
Nathan Ngo <nathanj238@gmail.com>
2024-10-24 21:59:11 +1100

    Finished up GridRailwayGraph implementation

---
+++ source/managers/overworld_manager/grid_railway_edge.gd
@@ -0,0 +1,6 @@
+extends RefCounted
+
+class_name GridRailwayEdge
+
+@export var name: String
+@export var weight := 1.0
---
+++ source/managers/overworld_manager/grid_railway_vertex.gd
@@ -0,0 +1,8 @@
+extends RefCounted
+
+class_name GridRailwayVertex
+
+# Array[Array[Vector2]]
+@export var connections = []
+
+# TODO: get_connections, add_connection, remove_connection
