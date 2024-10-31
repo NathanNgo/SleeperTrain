c78d6a501de1a3f3e4755b8a9fe221c90b44f5b6
Nathan Ngo <nathanj238@gmail.com>
2024-10-31 21:02:48 +1100

    Started on parallax backgrounds

--- source/world/background/background.gd
+++ source/world/background/background.gd
@@ -2,4 +2,4 @@ extends ParallaxBackground
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	scroll_offset -= Vector2(delta * 40, 0)
+	scroll_base_offset -= Vector2(delta * 40, 0)
