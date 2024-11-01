74c0f354984ef9004ed564ebc1c8da8c86c36173
Nathan Ngo <nathanj238@gmail.com>
2024-11-01 21:08:48 +1100

    Got parallax zooming working with some terrible code

--- source/world/background/background.gd
+++ source/world/background/background.gd
@@ -1,5 +1,29 @@
 extends ParallaxBackground
 
+
+@export var _layer_near: ParallaxLayer
+@export var _layer_far: ParallaxLayer
+var amount = 8
+
+
+func _ready() -> void:
+	_layer_near.show()
+	_layer_far.hide()
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	scroll_base_offset -= Vector2(delta * 40, 0)
+	scroll_base_offset -= Vector2(delta * 4, 0)
+
+
+func show_near_layers(scale_amount: float) -> void:
+	print(scale_amount)
+	print(scale_amount)
+	_layer_near.show()
+	_layer_far.hide()
+
+
+func hide_near_layers(scale_amount: float) -> void:
+	print(scale_amount)
+	_layer_near.hide()
+	_layer_far.show()
--- source/world/world.gd
+++ source/world/world.gd
@@ -1,1 +1,25 @@
 extends Node2D
+
+
+@export var _background: ParallaxBackground
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("ui_left"):
+		$Camera2D.position.x -= 10
+	if Input.is_action_pressed("ui_right"):
+		$Camera2D.position.x += 10
+	$BackgroundPivot.position = $Camera2D.position
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("zoom_in"):
+		$Camera2D.zoom = Vector2(2, 2)
+		$BackgroundPivot.scale = Vector2(0.5, 0.5)
+		$Camera2D.position.y += 160
+		_background.show_near_layers(1.5)
+	if event.is_action_pressed("zoom_out"):
+		$Camera2D.zoom = Vector2(1, 1)
+		$BackgroundPivot.scale = Vector2(1, 1)
+		$Camera2D.position.y -= 160
+		_background.hide_near_layers(1.5)
