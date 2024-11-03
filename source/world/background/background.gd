dcd1002303ebec58cb0acb987be2717d8ef41a3a
Nathan Ngo <nathanj238@gmail.com>
2024-11-03 22:11:43 +1100

    Finished parallax zooming and panning

--- source/world/background/background.gd
+++ source/world/background/background.gd
@@ -1,29 +1,25 @@
 extends ParallaxBackground
 
 
-@export var _layer_near: ParallaxLayer
-@export var _layer_far: ParallaxLayer
-var amount = 8
+@export var _small_layer: ParallaxLayer
+@export var _big_layer: ParallaxLayer
+
+const SCROLL_BASE_OFFSET_MAGNITUDE = 20
 
 
 func _ready() -> void:
-	_layer_near.show()
-	_layer_far.hide()
+	_big_layer.hide()
+	_small_layer.show()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	scroll_base_offset -= Vector2(delta * 4, 0)
+	scroll_base_offset -= Vector2(delta * SCROLL_BASE_OFFSET_MAGNITUDE, 0)
 
 
-func show_near_layers(scale_amount: float) -> void:
-	print(scale_amount)
-	print(scale_amount)
-	_layer_near.show()
-	_layer_far.hide()
-
-
-func hide_near_layers(scale_amount: float) -> void:
-	print(scale_amount)
-	_layer_near.hide()
-	_layer_far.show()
+func show_big_layer(showing := false) -> void:
+	if showing:
+		_small_layer.hide()
+		_big_layer.show()
+	else:
+		_small_layer.show()
+		_big_layer.hide()
--- source/world/world.gd
+++ source/world/world.gd
@@ -2,24 +2,33 @@ extends Node2D
 
 
 @export var _background: ParallaxBackground
+@export var _camera: Camera2D
+@export var _background_pivot: Node2D
+
+const CAMERA_PAN_SPEED = 5
+const CAMERA_ZOOM_INCREMENT = 2
+const CAMERA_ZOOM_OFFSET = 200
+const MAX_CAMERA_ZOOM = 2
+const MIN_CAMERA_ZOOM = 1
 
 
 func _process(_delta: float) -> void:
-	if Input.is_action_pressed("ui_left"):
-		$Camera2D.position.x -= 10
-	if Input.is_action_pressed("ui_right"):
-		$Camera2D.position.x += 10
-	$BackgroundPivot.position = $Camera2D.position
+	if Input.is_action_pressed("pan_left"):
+		_camera.position.x -= CAMERA_PAN_SPEED
+	if Input.is_action_pressed("pan_right"):
+		_camera.position.x += CAMERA_PAN_SPEED
+	_background_pivot.position = _camera.position
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("zoom_in"):
-		$Camera2D.zoom = Vector2(2, 2)
-		$BackgroundPivot.scale = Vector2(0.5, 0.5)
-		$Camera2D.position.y += 160
-		_background.show_near_layers(1.5)
-	if event.is_action_pressed("zoom_out"):
-		$Camera2D.zoom = Vector2(1, 1)
-		$BackgroundPivot.scale = Vector2(1, 1)
-		$Camera2D.position.y -= 160
-		_background.hide_near_layers(1.5)
+	if event.is_action_pressed("zoom_in") and _camera.zoom.x < MAX_CAMERA_ZOOM:
+		_camera.position.y += CAMERA_ZOOM_OFFSET
+		_camera.zoom *= Vector2.ONE * CAMERA_ZOOM_INCREMENT
+		_background_pivot.scale /= Vector2.ONE * CAMERA_ZOOM_INCREMENT
+		_background.show_big_layer(true)
+
+	if event.is_action_pressed("zoom_out") and _camera.zoom.x > MIN_CAMERA_ZOOM:
+		_camera.position.y -= CAMERA_ZOOM_OFFSET
+		_camera.zoom /= Vector2.ONE * CAMERA_ZOOM_INCREMENT
+		_background_pivot.scale *= Vector2.ONE * CAMERA_ZOOM_INCREMENT
+		_background.show_big_layer(false)
