7085ffed52ea667ec16773b4bd03a787a8bb5612
Nathan Ngo <nathanj238@gmail.com>
2024-11-25 17:42:42 +1100

    Ran parser on missing files

--- source/menus/resource_management_menu/resource_management_menu.gd
+++ source/menus/resource_management_menu/resource_management_menu.gd
@@ -22,94 +22,94 @@ const MIN_RESOURCE = 0
 @export var _train_resources: Resource
 
 var town_amount = {
-	Globals.ResourceType.COAL: 0, Globals.ResourceType.LUXURIES: 0, Globals.ResourceType.FOOD: 0
+    Globals.ResourceType.COAL: 0, Globals.ResourceType.LUXURIES: 0, Globals.ResourceType.FOOD: 0
 }
 var purchase_amount = {
-	Globals.ResourceType.COAL: 0, Globals.ResourceType.LUXURIES: 0, Globals.ResourceType.FOOD: 0
+    Globals.ResourceType.COAL: 0, Globals.ResourceType.LUXURIES: 0, Globals.ResourceType.FOOD: 0
 }
 
 
 func _ready() -> void:
-	set_current_train_resources()
-	_number_select_coal.display_label.text = str(0)
-	_number_select_luxuries.display_label.text = str(0)
-	_number_select_food.display_label.text = str(0)
-	_setup_number_select(_number_select_coal, Globals.ResourceType.COAL)
-	_setup_number_select(_number_select_luxuries, Globals.ResourceType.LUXURIES)
-	_setup_number_select(_number_select_food, Globals.ResourceType.FOOD)
-	_purchase_button.pressed.connect(_on_purchase_button_pressed)
+    set_current_train_resources()
+    _number_select_coal.display_label.text = str(0)
+    _number_select_luxuries.display_label.text = str(0)
+    _number_select_food.display_label.text = str(0)
+    _setup_number_select(_number_select_coal, Globals.ResourceType.COAL)
+    _setup_number_select(_number_select_luxuries, Globals.ResourceType.LUXURIES)
+    _setup_number_select(_number_select_food, Globals.ResourceType.FOOD)
+    _purchase_button.pressed.connect(_on_purchase_button_pressed)
 
 
 func set_current_train_resources() -> void:
-	_current_coal_label.text = str(_train_resources.resources[Globals.ResourceType.COAL])
-	_current_luxuries_label.text = str(_train_resources.resources[Globals.ResourceType.LUXURIES])
-	_current_food_label.text = str(_train_resources.resources[Globals.ResourceType.FOOD])
+    _current_coal_label.text = str(_train_resources.resources[Globals.ResourceType.COAL])
+    _current_luxuries_label.text = str(_train_resources.resources[Globals.ResourceType.LUXURIES])
+    _current_food_label.text = str(_train_resources.resources[Globals.ResourceType.FOOD])
 
 
 func set_current_town_resources(coal: int, luxuries: int, food: int) -> void:
-	town_amount[Globals.ResourceType.COAL] = coal
-	town_amount[Globals.ResourceType.LUXURIES] = luxuries
-	town_amount[Globals.ResourceType.FOOD] = food
-	_town_coal_label.text = str(coal)
-	_town_luxuries_label.text = str(luxuries)
-	_town_food_label.text = str(food)
+    town_amount[Globals.ResourceType.COAL] = coal
+    town_amount[Globals.ResourceType.LUXURIES] = luxuries
+    town_amount[Globals.ResourceType.FOOD] = food
+    _town_coal_label.text = str(coal)
+    _town_luxuries_label.text = str(luxuries)
+    _town_food_label.text = str(food)
 
 
 func _setup_number_select(
-	number_select: HBoxContainer, resource_type: Globals.ResourceType
+    number_select: HBoxContainer, resource_type: Globals.ResourceType
 ) -> void:
-	number_select.minus_button.pressed.connect(
-		_on_minus_button_pressed.bind(number_select, resource_type)
-	)
-	number_select.plus_button.pressed.connect(
-		_on_plus_button_pressed.bind(number_select, resource_type)
-	)
+    number_select.minus_button.pressed.connect(
+        _on_minus_button_pressed.bind(number_select, resource_type)
+    )
+    number_select.plus_button.pressed.connect(
+        _on_plus_button_pressed.bind(number_select, resource_type)
+    )
 
 
 func _on_minus_button_pressed(
-	number_select: HBoxContainer, resource_type: Globals.ResourceType
+    number_select: HBoxContainer, resource_type: Globals.ResourceType
 ) -> void:
-	purchase_amount[resource_type] = clamp(
-		purchase_amount[resource_type] - 1, MIN_RESOURCE, town_amount[resource_type]
-	)
-	number_select.display_label.text = str(purchase_amount[resource_type])
+    purchase_amount[resource_type] = clamp(
+        purchase_amount[resource_type] - 1, MIN_RESOURCE, town_amount[resource_type]
+    )
+    number_select.display_label.text = str(purchase_amount[resource_type])
 
 
 func _on_plus_button_pressed(
-	number_select: HBoxContainer, resource_type: Globals.ResourceType
+    number_select: HBoxContainer, resource_type: Globals.ResourceType
 ) -> void:
-	purchase_amount[resource_type] = clamp(
-		purchase_amount[resource_type] + 1, MIN_RESOURCE, town_amount[resource_type]
-	)
-	number_select.display_label.text = str(purchase_amount[resource_type])
+    purchase_amount[resource_type] = clamp(
+        purchase_amount[resource_type] + 1, MIN_RESOURCE, town_amount[resource_type]
+    )
+    number_select.display_label.text = str(purchase_amount[resource_type])
 
 
 func _on_purchase_button_pressed() -> void:
-	var coal_amount = purchase_amount[Globals.ResourceType.COAL]
-	var luxuries_amount = purchase_amount[Globals.ResourceType.LUXURIES]
-	var food_amount = purchase_amount[Globals.ResourceType.FOOD]
+    var coal_amount = purchase_amount[Globals.ResourceType.COAL]
+    var luxuries_amount = purchase_amount[Globals.ResourceType.LUXURIES]
+    var food_amount = purchase_amount[Globals.ResourceType.FOOD]
 
-	_train_resources.resources[Globals.ResourceType.COAL] += coal_amount
-	_train_resources.resources[Globals.ResourceType.LUXURIES] += luxuries_amount
-	_train_resources.resources[Globals.ResourceType.FOOD] += food_amount
+    _train_resources.resources[Globals.ResourceType.COAL] += coal_amount
+    _train_resources.resources[Globals.ResourceType.LUXURIES] += luxuries_amount
+    _train_resources.resources[Globals.ResourceType.FOOD] += food_amount
 
-	# FIXME:
-	# This can cause bugs as we now have 2 sources of truth. The menu, and the vertex.
-	# We shouldn't have town_amount. Just pass in the resource amount for the town.
-	resource_purchased.emit(Globals.ResourceType.COAL, coal_amount)
-	resource_purchased.emit(Globals.ResourceType.LUXURIES, luxuries_amount)
-	resource_purchased.emit(Globals.ResourceType.FOOD, food_amount)
+    # FIXME:
+    # This can cause bugs as we now have 2 sources of truth. The menu, and the vertex.
+    # We shouldn't have town_amount. Just pass in the resource amount for the town.
+    resource_purchased.emit(Globals.ResourceType.COAL, coal_amount)
+    resource_purchased.emit(Globals.ResourceType.LUXURIES, luxuries_amount)
+    resource_purchased.emit(Globals.ResourceType.FOOD, food_amount)
 
-	var new_town_coal = town_amount[Globals.ResourceType.COAL] - coal_amount
-	var new_town_luxuries = town_amount[Globals.ResourceType.LUXURIES] - luxuries_amount
-	var new_town_food = town_amount[Globals.ResourceType.FOOD] - food_amount
+    var new_town_coal = town_amount[Globals.ResourceType.COAL] - coal_amount
+    var new_town_luxuries = town_amount[Globals.ResourceType.LUXURIES] - luxuries_amount
+    var new_town_food = town_amount[Globals.ResourceType.FOOD] - food_amount
 
-	set_current_town_resources(new_town_coal, new_town_luxuries, new_town_food)
-	set_current_train_resources()
+    set_current_town_resources(new_town_coal, new_town_luxuries, new_town_food)
+    set_current_train_resources()
 
-	purchase_amount[Globals.ResourceType.COAL] = 0
-	purchase_amount[Globals.ResourceType.LUXURIES] = 0
-	purchase_amount[Globals.ResourceType.FOOD] = 0
-	_number_select_coal.display_label.text = str(0)
-	_number_select_luxuries.display_label.text = str(0)
-	_number_select_food.display_label.text = str(0)
+    purchase_amount[Globals.ResourceType.COAL] = 0
+    purchase_amount[Globals.ResourceType.LUXURIES] = 0
+    purchase_amount[Globals.ResourceType.FOOD] = 0
+    _number_select_coal.display_label.text = str(0)
+    _number_select_luxuries.display_label.text = str(0)
+    _number_select_food.display_label.text = str(0)
