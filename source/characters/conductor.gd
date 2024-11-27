a4bdfe3b4e1074222bf2e2562d426202e58240a3
Syfro <elboggerone@hotmail.com>
2024-11-27 17:52:08 +1100

    Added Conductor node + movement & animation

---
+++ source/characters/conductor.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("pan_right"):
+		if not Input.is_action_pressed("pan_left"):
+			conductor_go("right")
+		else:
+			conductor_go("stop")
+	if Input.is_action_just_released("pan_right"):
+		if not Input.is_action_pressed("pan_left"):
+			conductor_go("stop")
+		else:
+			conductor_go("left")
+	if Input.is_action_just_pressed("pan_left"):
+		if not Input.is_action_pressed("pan_right"):
+			conductor_go("left")
+		else:
+			conductor_go("stop")
+	if Input.is_action_just_released("pan_left"):
+		if not Input.is_action_pressed("pan_right"):
+			conductor_go("stop")
+		else:
+			conductor_go("right")
+
+func conductor_go(direction: String) -> void:
+	if direction == "right":
+		$AnimationPlayer.play("walk_right")
+	if direction == "left":
+		$AnimationPlayer.play("walk_left")
+	if direction == "stop":
+		$AnimationPlayer.stop()
+		$ConductorSprite.frame = 0
