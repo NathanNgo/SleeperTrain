45b51f6e4bcf75648ab299888d04772d700576d8
Syfro <elboggerone@hotmail.com>
2024-11-28 14:59:31 +1100

    Cleaned up Conductor code

--- source/characters/conductor.gd
+++ source/characters/conductor.gd
@@ -1,28 +1,27 @@
 extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("pan_right"):
-		if not Input.is_action_pressed("pan_left"):
-			conductor_go("right")
-		else:
-			conductor_go("stop")
-	if Input.is_action_just_released("pan_right"):
-		if not Input.is_action_pressed("pan_left"):
-			conductor_go("stop")
-		else:
-			conductor_go("left")
-	if Input.is_action_just_pressed("pan_left"):
-		if not Input.is_action_pressed("pan_right"):
-			conductor_go("left")
-		else:
-			conductor_go("stop")
-	if Input.is_action_just_released("pan_left"):
-		if not Input.is_action_pressed("pan_right"):
-			conductor_go("stop")
-		else:
-			conductor_go("right")
+	#if (Input.is_action_just_pressed("pan_right") and not Input.is_action_pressed("pan_left")) or Input.is_action_just_released("pan_left") and Input.is_action_pressed("pan_right"):
+		#move_conductor("right")
+		#else:
+			#move_conductor("stop")
+	#if Input.is_action_just_released("pan_right"):
+		#if not Input.is_action_pressed("pan_left"):
+			#move_conductor("stop")
+		#else:
+			#move_conductor("left")
+	#if Input.is_action_just_pressed("pan_left"):
+		#if not Input.is_action_pressed("pan_right"):
+			#move_conductor("left")
+		#else:
+			#move_conductor("stop")
+	#if Input.is_action_just_released("pan_left"):
+		#if not Input.is_action_pressed("pan_right"):
+			#move_conductor("stop")
+		#else:
+			#move_conductor("right")
 
-func conductor_go(direction: String) -> void:
+func move_conductor(direction: String) -> void:
 	if direction == "right":
 		$AnimationPlayer.play("walk_right")
 	if direction == "left":
