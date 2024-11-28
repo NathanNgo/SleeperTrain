24e017c237ebdec6093d0fb8d6c24c6664d24209
Syfro <elboggerone@hotmail.com>
2024-11-28 16:06:41 +1100

    Cleaned up Conductor code.

--- source/characters/conductor.gd
+++ source/characters/conductor.gd
@@ -1,31 +1,21 @@
 extends CharacterBody2D
 
+@export var conductor_sprite: Sprite2D
+@export var conductor_animation: AnimationPlayer
+
 func _physics_process(delta: float) -> void:
-	#if (Input.is_action_just_pressed("pan_right") and not Input.is_action_pressed("pan_left")) or Input.is_action_just_released("pan_left") and Input.is_action_pressed("pan_right"):
-		#move_conductor("right")
-		#else:
-			#move_conductor("stop")
-	#if Input.is_action_just_released("pan_right"):
-		#if not Input.is_action_pressed("pan_left"):
-			#move_conductor("stop")
-		#else:
-			#move_conductor("left")
-	#if Input.is_action_just_pressed("pan_left"):
-		#if not Input.is_action_pressed("pan_right"):
-			#move_conductor("left")
-		#else:
-			#move_conductor("stop")
-	#if Input.is_action_just_released("pan_left"):
-		#if not Input.is_action_pressed("pan_right"):
-			#move_conductor("stop")
-		#else:
-			#move_conductor("right")
+	if (Input.is_action_just_pressed("pan_right") and not Input.is_action_pressed("pan_left")) or (Input.is_action_just_released("pan_left") and Input.is_action_pressed("pan_right")):
+		move_conductor("right")
+	if (Input.is_action_just_pressed("pan_left") and not Input.is_action_pressed("pan_right")) or (Input.is_action_just_released("pan_right") and Input.is_action_pressed("pan_left")):
+		move_conductor("left")
+	if (Input.is_action_pressed("pan_left") and Input.is_action_pressed("pan_right")) or (Input.is_action_just_released("pan_left") and not Input.is_action_pressed("pan_right")) or (Input.is_action_just_released("pan_right") and not Input.is_action_pressed("pan_left")):
+		move_conductor("stop")
 
 func move_conductor(direction: String) -> void:
 	if direction == "right":
-		$AnimationPlayer.play("walk_right")
+		conductor_animation.play("walk_right")
 	if direction == "left":
-		$AnimationPlayer.play("walk_left")
+		conductor_animation.play("walk_left")
 	if direction == "stop":
-		$AnimationPlayer.stop()
-		$ConductorSprite.frame = 0
+		conductor_animation.stop()
+		conductor_sprite.frame = 0
