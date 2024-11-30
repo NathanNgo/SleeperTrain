ab3596645e6b0a1f75c79928714400786708eb2f
Syfro <elboggerone@hotmail.com>
2024-11-30 14:13:15 +1100

    Update train_carriage.gd

--- source/world/world_objects/train_carriages/train_carriage.gd
+++ source/world/world_objects/train_carriages/train_carriage.gd
@@ -11,11 +11,11 @@ var carriage_id: int
 
 
 
-func _on_train_carriage_area_body_entered(body: Node2D) -> void:
+func _on_train_carriage_area_body_entered(_body: Node2D) -> void:
 	player_entered_carriage.emit(self)
 	print("Detected player entering")
 
 
-func _on_train_carriage_area_body_exited(body: Node2D) -> void:
+func _on_train_carriage_area_body_exited(_body: Node2D) -> void:
 	player_exited_carriage.emit(self)
 	print("Detected player leaving")
