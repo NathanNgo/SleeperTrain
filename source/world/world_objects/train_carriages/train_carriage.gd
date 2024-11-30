46ae557d2801e983e75f50cdc9bb2b54b2fa12f7
Syfro <elboggerone@hotmail.com>
2024-11-30 14:12:37 +1100

    Update train_carriage.gd

--- source/world/world_objects/train_carriages/train_carriage.gd
+++ source/world/world_objects/train_carriages/train_carriage.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
+signal player_entered_carriage(Node2D)
+signal player_exited_carriage(Node2D)
+
 @export var _train_carriage_shape: CollisionShape2D
 
 var carriage_id: int
 
 @onready var train_carriage_length := _train_carriage_shape.shape.get_rect().size.x
 
-signal player_entered_carriage(Node2D)
-signal player_exited_carriage(Node2D)
+
 
 func _on_train_carriage_area_body_entered(body: Node2D) -> void:
 	player_entered_carriage.emit(self)
