eaca4123c2c5e83bc667e6edf195ac7cf4bf5bda
Nathan Ngo <nathanj238@gmail.com>
2024-12-13 19:47:03 +1100

    Reformatted

--- source/world/world_objects/train_carriages/train_carriage.gd
+++ source/world/world_objects/train_carriages/train_carriage.gd
@@ -10,36 +10,36 @@ var carriage_divisions = []
 
 
 func add_division_on_level(division_position: int, level: int) -> void:
-    # level > size, discontinuous array.
-    # level == size, creating a new item in the array by appending onto the end.
-    # level < size, accessing existing item.
+	# level > size, discontinuous array.
+	# level == size, creating a new item in the array by appending onto the end.
+	# level < size, accessing existing item.
 
-    if level > carriage_divisions.size():
-        # TODO: Error out or show to player.
-        print("Cannot create discontinuous level")
-        return
+	if level > carriage_divisions.size():
+		# TODO: Error out or show to player.
+		print("Cannot create discontinuous level")
+		return
 
-    if level == carriage_divisions.size():
-        carriage_divisions.append([])
+	if level == carriage_divisions.size():
+		carriage_divisions.append([])
 
-    if carriage_divisions[level].has(division_position):
-        # TODO: Error out or show to player.
-        print("Division already exists on level")
-        return
+	if carriage_divisions[level].has(division_position):
+		# TODO: Error out or show to player.
+		print("Division already exists on level")
+		return
 
-    carriage_divisions[level].append(division_position)
-    carriage_divisions.sort()
+	carriage_divisions[level].append(division_position)
+	carriage_divisions.sort()
 
 
 func remove_division_on_level(division_position: int, level: int) -> void:
-    if level >= carriage_divisions.size():
-        # TODO: Error out or show to player.
-        print("Level does not exist")
-        return
+	if level >= carriage_divisions.size():
+		# TODO: Error out or show to player.
+		print("Level does not exist")
+		return
 
-    carriage_divisions[level].erase(division_position)
-    carriage_divisions.sort()
+	carriage_divisions[level].erase(division_position)
+	carriage_divisions.sort()
 
 
 func remove_all_divisions() -> void:
-    carriage_divisions.clear()
+	carriage_divisions.clear()
